e4681ef42794f5a310de0492c57a58c8de691584
Akhmed Ramzanov <ami0013@yandex.ru>
2022-02-14 22:51:43 +0300

    Fix

--- 1.JavaSyntax/src/com/javarush/task/pro/task10/task1010/Test.java
+++ 1.JavaSyntax/src/com/javarush/task/pro/task10/task1010/Test.java
@@ -5,6 +5,6 @@ public class Test {
         String s1 = new String("Hello, World!!!");
         String s2 = new String("Hello, World!!!");
         System.out.println(s1.equals(s2) ? "Равны" : "Не равны");
-        System.out.println("Hello, World!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
+        System.out.println("Hello, World!!!!!!!!!");
     }
 }
